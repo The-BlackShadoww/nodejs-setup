a94fb58a4e20f8da02d9c0bf18caad53329f7f21
Ashikur Rahman <ashikur.jebu111@gmail.com>
2025-09-22 22:24:30 +0600

    refactor: update multer storage configuration to use OS-specific temp directory

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,11 @@
 import multer from "multer";
+import os from "os";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    // cb(null, "./public/temp");
+    // cb(null, "/tmp");
+    cb(null, os.tmpdir()); //! Node handles the correct path for each OS
   },
 
   filename: function (req, file, cb) {
@@ -16,3 +19,23 @@ const storage = multer.diskStorage({
 });
 
 export const upload = multer({ storage: storage });
+
+
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+
+//   //   filename: function (req, file, cb) {
+//   //     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//   //     cb(null, file.fieldname + "-" + uniqueSuffix);
+//   //   },
+// });
+
+// export const upload = multer({ storage: storage });
